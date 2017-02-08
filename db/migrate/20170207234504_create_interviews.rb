fdad4356c37097e2a3fbd74e57d4704a6ba0e902
Mark Cooper <markchristophercooper@gmail.com>
2017-02-07 16:55:25 -0800

    Add string date expression

--- db/migrate/20170207234504_create_interviews.rb
+++ db/migrate/20170207234504_create_interviews.rb
@@ -2,7 +2,11 @@ class CreateInterviews < ActiveRecord::Migration[5.0]
   def change
     create_table :interviews do |t|
       t.references :record, index: true
+      # 'persdata' 'Date of interview'
       t.datetime   :date
+      # $245f
+      t.string     :date_expression
+      # 'persdata' 'Length of interview'
       t.string     :length
       t.timestamps
     end
