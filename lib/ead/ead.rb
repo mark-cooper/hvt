b65987162144b92f8dad25475b44c4fb30e43a2f
Mark Cooper <markchristophercooper@gmail.com>
2017-05-01 17:36:02 -0700

    Origination unique names only

--- lib/ead/ead.rb
+++ lib/ead/ead.rb
@@ -132,7 +132,7 @@ module EAD
       originations.concat interviewees
       originations.concat interviewers
     end
-    originations
+    originations.uniq { |origination| origination[:name] }
   end
 
   def self.get_related_materials(record)
